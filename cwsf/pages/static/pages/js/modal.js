f7c1271f859a4dbf8c5031371c257ac5a1ec95bf
Ethan Zhao <ethanzhao1981@gmail.com>
2025-07-03 14:44:13 -0400

    update ethan modal summaries

--- cwsf/pages/static/pages/js/modal.js
+++ cwsf/pages/static/pages/js/modal.js
@@ -1,12 +1,16 @@
 const summaries = {
-  "Louis Wu": "Our first team member specializes in biology, behavioral sciences, and product design. He is a Remarkable Students Showcase Champion, won gold at the Canada-Wide Science Fair, and was chosen to represent Canada at the EU Contest for Young Scientists.",
-  "Koral Kulacoglu": "Koral specializes in engineering, programming, and neural networks. Alongside Louis, Koral was one of the original developers of the FourSight project.",
-  "Ethan Zhao": "Ethan specializes in web design and production. He manages website design, video editing, and video design.",
-  "Alex Gan": "Alex specializes in finance, business management, and corporate development. Alex worked for the Ontario Public Service as a Finance & Risk Assistant and is now focused on corporate development of FourSight."
+  'Louis Wu':
+    'Our first team member specializes in biology, behavioral sciences, and product design. He is a Remarkable Students Showcase Champion, won gold at the Canada-Wide Science Fair, and was chosen to represent Canada at the EU Contest for Young Scientists.',
+  'Koral Kulacoglu':
+    'Koral specializes in engineering, programming, and neural networks. Alongside Louis, Koral was one of the original developers of the FourSight project.',
+  'Ethan Zhao':
+    'Ethan currently studies mechatronics engineering and business administration with a specialization in software. Starting in grade 7, Ethan has created projects have still remain active to this day. He manages website design, development, and video production for FourSight.',
+  'Alex Gan':
+    'Alex specializes in finance, business management, and corporate development. Alex worked for the Ontario Public Service as a Finance & Risk Assistant and is now focused on corporate development of FourSight.',
 };
 
 function getPerson(name) {
-  let summary = summaries[name] ?? "Unknown Name";
+  let summary = summaries[name] ?? 'Unknown Name';
   $('#modal-text').html(summary);
   $('#modal').show();
   $('#modal-content').css('opacity', '0');
@@ -14,7 +18,7 @@ function getPerson(name) {
     {
       opacity: 1,
     },
-    100
+    100,
   );
 }
 
